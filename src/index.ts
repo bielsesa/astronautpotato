3e3c172039dcf9c3c9a67699e4148b1694071b03
Biel Serrano Sanchez <bielsesa@gmail.com>
2022-07-21 16:53:33 +0200

    added filters and changed page title

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Container, Sprite } from 'pixi.js'
+import { GlitchFilter } from 'pixi-filters'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +10,31 @@ const app = new Application({
 	height: 480
 });
 
+const conty: Container = new Container();
+
+conty.x = app.screen.width / 2;
+conty.y = app.screen.height / 2;
+app.stage.addChild(conty);
+
 const clampy: Sprite = Sprite.from("clampy.png");
 
 clampy.anchor.set(0.5);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+clampy.x = 0;
+clampy.y = 0;
 
-app.stage.addChild(clampy);
+conty.rotation = Math.PI * 0.5;
+
+const glitchFilter: GlitchFilter = new GlitchFilter({
+	seed: 0.3,
+	slices: 10,
+	offset: 10,
+	red: [2, 2],
+	blue: [10, -4],
+	green: [-10, 4]
+});
+
+
+clampy.filters = [glitchFilter];
+
+conty.addChild(clampy);
